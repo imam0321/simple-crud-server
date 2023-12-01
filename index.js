0f8101d708c74033c465077eb850bbbc69d33bd1
imam0321 <imam.hossain0321@gmail.com>
2023-12-02 03:19:43 +0600

    data post and get the data

--- index.js
+++ index.js
@@ -22,9 +22,24 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const coffeeCollection = client.db('coffee').collection('coffee');
+
+    app.post('/coffee', async(req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+    app.get('/coffee', async(req, res)=> {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
